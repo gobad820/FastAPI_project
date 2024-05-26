f5394950f162f72d6168aa76dfc602bbbc31c9d5
Kim Sanghae <pelikan@pusan.ac.kr>
2024-05-27 01:40:47 +0900

    Fix: Initialize the project

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Router from 'svelte-spa-router';
+    import Home from "./routes/Home.svelte";
+    import Detail from "./routes/Detail.svelte"
+    import Todo from "./routes/Todo.svelte";
+    import TodoCreate from "./routes/TodoCreate.svelte";
+    import Weather from './components/Weather.svelte';
+
+    const routes = {
+        "/": Todo,
+        "/detail/:todo_id":Detail,
+        '/todo-create': TodoCreate
+    }
+</script>
+<Weather />
+<Router {routes}/>
